ee70e347e5eeaed3c0a86cc7dee6ab0e4ba48d57
Suresh <sureshdevopsgcp@gmail.com>
2023-09-24 17:06:38 +0530

    Updated the deployed Image and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,17 @@ export const metadata: Metadata = {
   other: {
     "theme-color": "#0d1117",
     "color-scheme": "dark only",
+    "og:title": "Project Showcase",
+    "og:description": "Project Portfolio",
+    "og:image":
+      "https://pbs.twimg.com/media/F6yaxc-W8AA5nhy?format=png&name=small",
+    "og:url": "https://project-showcase-eight.vercel.app",
     "og:type": "website",
+    "twitter:title": "Project Showcase",
+    "twitter:url": "https://project-showcase-eight.vercel.app",
+    "twitter:description": "Project Portfolio",
+    "twitter:image":
+      "https://pbs.twimg.com/media/F6yaxc-W8AA5nhy?format=png&name=small",
   },
 };
 
